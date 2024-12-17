e9ede630b1e601c3c6454d832ae77619eb4d86de
Amr Abdelwahed <abdelwa0@sheridancollege.ca>
2024-12-17 13:27:48 -0500

    added resume

--- src/data/constants.js
+++ src/data/constants.js
@@ -7,7 +7,7 @@ export const Bio = {
       "Full Stack Developer.",
     ],
     github: "https://github.com/amrabdelwahed",
-    resume: "https://drive.google.com/file/d/1xYA2dVmPT8MILHAbwLWFriIIFzvWzkCC/view?usp=sharing",
+    resume: "https://drive.google.com/file/d/1gSOYhPAP9Zl9uo39kHXBTspHG4PBeC06/view?usp=sharing",
     linkedin: "https://www.linkedin.com/in/amrabdelwahed/",
   };
   
